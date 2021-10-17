0a55ec552de192693f5e35b1115e15254a75f9d0
gonzocode <tomasgons@gmail.com>
2021-10-17 17:37:13 +0200

    changed tempo

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             // wait for a while and call this function again for next character
             setTimeout(function () {
                 typeWriter(text, i + 1, fnCallback);
-            }, 50);
+            }, 60);
         }
         // text finished, call callback if there is a callback function
         else if (typeof fnCallback == "function") {
